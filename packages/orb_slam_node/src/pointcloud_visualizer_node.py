7118f405e644abd06bfa3dfb0fc71569d9621907
Jesse Coenraad <jessecoenraad2004@gmail.com>
2026-05-11 11:05:36 +0200

    update

--- packages/orb_slam_node/src/pointcloud_visualizer_node.py
+++ packages/orb_slam_node/src/pointcloud_visualizer_node.py
@@ -31,7 +31,7 @@ try:
     _BACKEND = "open3d"
 except ImportError:
     import matplotlib
-    matplotlib.use("TkAgg")
+    matplotlib.use("Qt5Agg")
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
     _BACKEND = "matplotlib"
